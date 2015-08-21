70e943c2e330049d8090f91790c3b68da46fd95c
nuchchan1234 <minkung_min@hotmail.com>
2015-08-21 18:27:20 +0700

    edit commandl

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -22,7 +22,10 @@ namespace HW1
         }
         static void Main(string[] args)
         {
-            int j = 5;
+            int j;
+            Console.WriteLine("input: ");
+            j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("output: {0}", process(j));
             Console.WriteLine(process(j));
             Console.ReadKey();
         }
